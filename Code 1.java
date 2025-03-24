8fb78bab5e3a9c02d48bd19910119da18dd60061
Santhosh040206 <itsmesanthosh.guru@gmail.com>
2025-03-24 17:48:08 +0530

    Add files via upload

---
+++ Code 1.java
@@ -0,0 +1,13 @@
+class Tester {
+	public static void main(String[] args) {
+        String name = "Sandy";
+        String street = "453 Side St.";
+        String city = "Virudhunagar";
+        String zipCode = "626001";
+        System.out.println("Address Details:");
+        System.out.println("Name: " + name);
+        System.out.println("Street: " + street);
+        System.out.println("City: " + city);
+        System.out.println("Zip Code: " + zipCode);
+	}
+}
